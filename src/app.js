324e2c86f4bc8506c0d58a50eb0e4686165e80cf
Faizy Broski <faizanhashmi5813@gmail.com>
2025-12-23 02:26:34 +0500

    Push

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express = require("express")
 const cors = require("cors")
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://lacarta-two.vercel.app"
+]
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
+    credentials: true
+  })
+)
+
 const app = express()
 
 app.use(cors())

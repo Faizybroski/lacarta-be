8110e7aad4b861ad24b338a29ed85a56dac45df1
Faizy Broski <faizanhashmi5813@gmail.com>
2025-12-24 04:41:00 +0500

    Auth APIs

--- api/server.js
+++ api/server.js
@@ -1,7 +1,17 @@
-const app = require("../src/app")
+import dotenv from "dotenv";
+import app from "../src/app.js";
+import connectDB from "../src/Config/db.js";
+import authRoutes from "../src/Routes/authRoutes.js";
 
-const PORT = process.env.PORT || 5000
+dotenv.config();
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
+
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
--- src/app.js
+++ src/app.js
@@ -1,32 +1,28 @@
-const express = require("express")
-const cors = require("cors")
+import express from "express";
+import cors from "cors";
 
+const app = express();
+
+/* Allowed Origins */
 const allowedOrigins = [
   "http://localhost:5173",
   "https://lacarta-two.vercel.app"
-]
+];
 
-const app = express()
+/* Middlewares */
+app.use(express.json());
 
 app.use(
   cors({
     origin: function (origin, callback) {
       if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true)
+        callback(null, true);
       } else {
-        callback(new Error("Not allowed by CORS"))
+        callback(new Error("Not allowed by CORS"));
       }
     },
-    credentials: true
+    credentials: true,
   })
-)
+);
 
-
-// app.use(cors())
-app.use(express.json())
-
-app.get("/", (req, res) => {
-  res.json({ message: "App is ready" })
-})
-
-module.exports = app
+export default app;
